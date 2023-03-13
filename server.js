5fd120285cbca26fb4c25cd209c8c2c8fc1c5b7a
TOM LECOMTE (Etudiant) <lecomto1@ur.univ-rouen.fr>
2023-03-13 11:27:21 +0100

    version boostrap

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ let port = 3000;
 
 app.use(express.static(__dirname + '/www'));
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.listen(port, ()=>{
     console.log('Le serveur est en route');
     console.log(`Serveur listening at http://localhost:${port}`);
@@ -12,4 +17,8 @@ app.listen(port, ()=>{
 
 app.get('/', (req, res, next) => {
     res.sendFile('/www/index.html');
+})
+
+app.get('/accueil', (req, res, next) => {
+    res.sendFile('/www/accueil.html');
 })
